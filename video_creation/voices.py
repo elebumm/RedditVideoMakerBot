477f69261d5d4a570b5594fe8b05f5f4d74cf614
lilkitkat1 <wellmev.viggo@gmail.com>
2022-06-01 19:13:31 +0200

    Customize voice

--- video_creation/voices.py
+++ video_creation/voices.py
@@ -3,7 +3,27 @@ from pathlib import Path
 from mutagen.mp3 import MP3
 from utils.console import print_step, print_substep
 from rich.progress import track
+from sys import argv
 
+def get_accent(_in: str) -> str:
+    """Returns the short version of an accent.
+       For example: _in = "English (Australia)" -> "com.au".
+
+    Args:
+        _in: the input text. For example "Australia"
+    """
+
+    accents = {
+        "australia": "com.au",
+        "united kingdom": "co.uk",
+        "united states": "com",
+        "canada": "ca",
+        "india" : "co.in",
+        "ireland": "ie",
+        "south africa": "co.za"
+    }
+
+    return accents[_in.lower()]
 
 def save_text_to_mp3(reddit_obj):
     """Saves Text to MP3 files.
@@ -11,13 +31,19 @@ def save_text_to_mp3(reddit_obj):
     Args:
         reddit_obj : The reddit object you received from the reddit API in the askreddit.py file.
     """
+    try:
+        accent = get_accent(argv[1])
+    # If no argument; set to default (co.uk)
+    except IndexError:
+        accent = "co.uk"
+
     print_step("Saving Text to MP3 files 🎶")
     length = 0
 
     # Create a folder for the mp3 files.
     Path("assets/mp3").mkdir(parents=True, exist_ok=True)
 
-    tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld="co.uk")
+    tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False, tld = accent)
     tts.save(f"assets/mp3/title.mp3")
     length += MP3(f"assets/mp3/title.mp3").info.length
 
@@ -25,7 +51,7 @@ def save_text_to_mp3(reddit_obj):
         # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer, but this is just a good starting point
         if length > 50:
             break
-        tts = gTTS(text=comment["comment_body"], lang="en")
+        tts = gTTS(text=comment["comment_body"], lang="en", tld = accent)
         tts.save(f"assets/mp3/{idx}.mp3")
         length += MP3(f"assets/mp3/{idx}.mp3").info.length
 
