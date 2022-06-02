93bcd7be772d565be7e893f336fe5a4e3cce107d
Kamushy <92086533+Kamushy@users.noreply.github.com>
2022-06-02 22:57:02 +1000

    file will now go to correct folder

--- video_creation/final_video.py
+++ video_creation/final_video.py
@@ -56,7 +56,7 @@ def make_final_video(number_of_clips):
     )
     image_concat.audio = audio_composite
     final = CompositeVideoClip([background_clip, image_concat])
-    filename = (re.sub('[?\"/%*:|<>]', '', ("assets/" + reddit.askreddit.submission.title + ".mp4")))
+    filename = (re.sub('[?\"%*:|<>]', '', ("assets/" + reddit.askreddit.submission.title + ".mp4")))
     final.write_videofile(filename, fps=30, audio_codec="aac", audio_bitrate="192k")
     for i in range(0, number_of_clips):
         pass
