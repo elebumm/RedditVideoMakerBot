f05eff4f88402b53c1dc919c6e793dea628c3da3
Callum Leslie <github@cleslie.uk>
2022-06-04 01:30:10 +0100

    Alert user if their .env is not configured properly

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from utils.console import print_markdown
-import time
-
 from reddit.subreddit import get_subreddit_threads
 from video_creation.background import download_background, chop_background_video
 from video_creation.voices import save_text_to_mp3
 from video_creation.screenshot_downloader import download_screenshots_of_reddit_posts
 from video_creation.final_video import make_final_video
 from dotenv import load_dotenv
-import os
+import os, time
+
+REQUIRED_VALUES = ["REDDIT_CLIENT_ID","REDDIT_CLIENT_SECRET","REDDIT_USERNAME","REDDIT_PASSWORD"]
 
 print_markdown(
     "### Thanks for using this tool! [Feel free to contribute to this project on GitHub!](https://lewismenelaws.com) If you have any questions, feel free to reach out to me on Twitter or submit a GitHub issue."
@@ -15,10 +15,17 @@ print_markdown(
 
 time.sleep(3)
 
-
-reddit_object = get_subreddit_threads()
-
 load_dotenv()
+
+configured = True
+
+for val in REQUIRED_VALUES:
+    if val not in os.environ or not os.getenv(val):
+        print(f"Please set the variable \"{val}\" in your .env file.")
+        configured = False
+
+if configured:
+reddit_object = get_subreddit_threads()
 length, number_of_comments = save_text_to_mp3(reddit_object)
 download_screenshots_of_reddit_posts(reddit_object, number_of_comments, os.getenv("THEME", "light"))
 download_background()
