3a7d91f474de5bbfdab0113b8479b0378949c943
Callum Leslie <github@cleslie.uk>
2022-06-04 01:41:48 +0100

    Create a .env file if none exists on run
    Should fix issues users have with creating their own .env file, by simply copying our template to a file for them.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from video_creation.voices import save_text_to_mp3
 from video_creation.screenshot_downloader import download_screenshots_of_reddit_posts
 from video_creation.final_video import make_final_video
 from dotenv import load_dotenv
-import os, time
+import os, time, shutil
 
 REQUIRED_VALUES = ["REDDIT_CLIENT_ID","REDDIT_CLIENT_SECRET","REDDIT_USERNAME","REDDIT_PASSWORD"]
 
@@ -19,15 +19,19 @@ load_dotenv()
 
 configured = True
 
+if not os.path.exists(".env"):
+    shutil.copy(".env.template", ".env")
+    configured = False
+
 for val in REQUIRED_VALUES:
     if val not in os.environ or not os.getenv(val):
         print(f"Please set the variable \"{val}\" in your .env file.")
         configured = False
 
 if configured:
-reddit_object = get_subreddit_threads()
-length, number_of_comments = save_text_to_mp3(reddit_object)
-download_screenshots_of_reddit_posts(reddit_object, number_of_comments, os.getenv("THEME", "light"))
-download_background()
-chop_background_video(length)
-final_video = make_final_video(number_of_comments)
+    reddit_object = get_subreddit_threads()
+    length, number_of_comments = save_text_to_mp3(reddit_object)
+    download_screenshots_of_reddit_posts(reddit_object, number_of_comments, os.getenv("THEME", "light"))
+    download_background()
+    chop_background_video(length)
+    final_video = make_final_video(number_of_comments)
