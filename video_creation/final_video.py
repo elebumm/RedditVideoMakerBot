b592f09ac552af10c25e722bffe61ef4cd700ff9
Jacob Chambers <jchambers1@protonmail.com>
2022-06-04 01:12:24 +0100

    Customised path for loops

--- video_creation/final_video.py
+++ video_creation/final_video.py
@@ -13,7 +13,7 @@ from utils.console import print_step
 W, H = 1080, 1920
 
 
-def make_final_video(number_of_clips):
+def make_final_video(number_of_clips, name):
     print_step("Creating the final video...")
     VideoFileClip.reW = lambda clip: clip.resize(width=W)
     VideoFileClip.reH = lambda clip: clip.resize(width=H)
@@ -54,7 +54,7 @@ def make_final_video(number_of_clips):
     image_concat.audio = audio_composite
     final = CompositeVideoClip([background_clip, image_concat])
     final.write_videofile(
-        "assets/final_video.mp4", fps=30, audio_codec="aac", audio_bitrate="192k"
+        "output/" + name + ".mp4", fps=30, audio_codec="aac", audio_bitrate="64k"
     )
 
     for i in range(0, number_of_clips):
