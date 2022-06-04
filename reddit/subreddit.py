87d211281220ea529e6b48008e884a2adb7cd5f9
Callum Leslie <github@cleslie.uk>
2022-06-04 01:40:55 +0100

    Move to casefold

--- reddit/subreddit.py
+++ reddit/subreddit.py
@@ -15,7 +15,7 @@ def get_subreddit_threads():
 
     print_step("Getting AskReddit threads...")
 
-    if os.getenv("REDDIT_2FA", default="no").lower() == "yes":
+    if os.getenv("REDDIT_2FA", default="no").casefold() == "yes":
         print(
             "\nEnter your two-factor authentication code from your authenticator app.\n"
         )
