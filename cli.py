64630b423b3332b602e56abbb13b4a1cc797a7e7
iaacornus <iaacornus.devel@gmail.com>
2022-06-04 23:34:59 +0800

    removed unncessary parameters and include the function calls

--- cli.py
+++ cli.py
@@ -1,5 +1,8 @@
 import argparse
 
+from main import main
+from utils.console import print_substep
+
 
 def program_options():
     description = """\
@@ -17,35 +20,32 @@ def program_options():
         help="Create a video.",
         action="store_true"
     )
-    # this one accepts link as input, as well as already downloaded video,
-    # defaults to the minecraft video.
-    parser.add_argument(
-        "-b",
-        "--background",
-        help="Use another video background for video (accepts link).",
-        action="store"
-    )
     parser.add_argument( # only accepts the name of subreddit, not links.
         "-S",
         "--subreddit",
         help="Use another sub-reddit.",
         action="store"
     )
-    # show most of the background processes of the program
+    # this one accepts link as input, defaults to the minecraft video
+    # does not accept file or already downloaded background yet.
     parser.add_argument(
-        "-v",
-        "--verbose",
-        help="Show the processes of program.",
-        action="store_true"
+        "-b",
+        "--background",
+        help="Use another video background for video (accepts link).",
+        action="store"
     )
 
     args = parser.parse_args()
 
     try:
-        ...
+        if args.create:
+            main(args.subreddit, args.background)
+        else:
+            print_substep("Error occured!", style="bold red")
+            raise SystemExit()
     except (
         ConnectionError,
-        KeyboardInterrupt
+        KeyboardInterrupt,
     ):
         ...
 
