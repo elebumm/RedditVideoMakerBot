cb3f825014b50a2c5e1f9e41b7d2ae8f3da2f388
Jason <thebosssg19@gmail.com>
2022-06-06 18:11:52 -0400

    updated voice.py with uri removal too

--- utils/voice.py
+++ utils/voice.py
@@ -4,13 +4,18 @@ import re
 def sanitize_text(text):
     """
     Sanitizes the text for tts.
-    What gets removed:
+       What gets removed:
     - following characters`^_~@!&;#:-%“”‘"%*/{}[]()\|<>?=+`
+    - any http or https links
     """
+
+    # remove any urls from the text
+    regex_urls = r"((http|https)://[^\s]+)"
+    result = re.sub(regex_urls, " ", text)
 
     # note: not removing apostrophes
     regex_expr = r"\s['|’]|['|’]\s|[\^_~@!&;#:\-%“”‘\"%\*/{}\[\]\(\)\\|<>=+]"
-    result = re.sub(regex_expr, " ", text)
+    result = re.sub(regex_expr, " ", result)
 
     # remove extra whitespace
     return " ".join(result.split())
