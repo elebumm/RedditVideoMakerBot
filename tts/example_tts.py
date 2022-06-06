84a26514005f7e234e4edafa861d2fac557125d8
Callum Leslie <github@cleslie.uk>
2022-06-06 17:58:06 +0100

    Example of how to implement a custom TTS provider

---
+++ tts/example_tts.py
@@ -0,0 +1,9 @@
+## Your TTS imports, etc.
+
+# def run(text, filepath):
+#     Call your TTS on the text variable
+#     ...
+#     Save your TTS to the a file using the filepath variable. The engine assumes it will be an mp3 file.
+#     ...
+#
+# any extra functions you need
