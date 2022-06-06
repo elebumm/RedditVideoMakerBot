49e9c1c997bf7290a9ee431c8c68a168c18523c2
iaacornus <iaacornus.devel@gmail.com>
2022-06-06 18:16:26 +0800

    some improvements and fixes to the merge

--- video_creation/final_video.py
+++ video_creation/final_video.py
@@ -52,30 +52,40 @@ def make_final_video(number_of_clips, file_name):
         audio_clips.append(AudioFileClip(f"assets/mp3/{i}.mp3"))
 
     audio_clips.insert(0, AudioFileClip("assets/mp3/title.mp3"))
+    try:
+        audio_clips.insert(1, AudioFileClip(f"assets/mp3/posttext.mp3"))
+    except (
+            OSError,
+            FileNotFoundError,
+        ):
+        ...
+
     audio_concat = concatenate_audioclips(audio_clips)
     audio_composite = CompositeAudioClip([audio_concat])
 
     # Gather all images
     image_clips = []
-    for i in range(0, number_of_clips):
-        image_clips.append(
-            ImageClip(f"assets/png/comment_{i}.png")
-            .set_duration(audio_clips[i + 1].duration)
+    if os.path.exists(f"assets/mp3/posttext.mp3"):
+        image_clips.insert(
+            0,
+            ImageClip(f"assets/png/title.png")
+            .set_duration(audio_clips[0].duration + audio_clips[1].duration)
             .set_position("center")
             .resize(width=W - 100)
-            .set_opacity(opacity),
-        )
+            .set_opacity(float(opacity)),
+            )
+    else:
+        image_clips.insert(
+            0,
+            ImageClip(f"assets/png/title.png")
+            .set_duration(audio_clips[0].duration)
+            .set_position("center")
+            .resize(width=W - 100)
+            .set_opacity(float(opacity)),
+            )
 
-    image_clips.insert(
-        0,
-        ImageClip("assets/png/title.png")
-        .set_duration(audio_clips[0].duration)
-        .set_position("center")
-        .resize(width=W - 100)
-        .set_opacity(opacity),
-    )
-    image_concat = concatenate_videoclips(image_clips).set_position(
-        ("center", "center")
+    image_concat = concatenate_videoclips(
+        image_clips).set_position(("center", "center")
     )
     image_concat.audio = audio_composite
     final = CompositeVideoClip([background_clip, image_concat])
