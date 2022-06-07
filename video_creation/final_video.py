6f243bc0e4cb0fa27f50af6b06395575ed6f1856
iaacornus <iaacornus.devel@gmail.com>
2022-06-07 13:28:26 +0800

    refactor and merge with develop branch

--- video_creation/final_video.py
+++ video_creation/final_video.py
@@ -61,36 +61,26 @@ def make_final_video(number_of_clips, file_name):
         audio_concat = concatenate_audioclips(audio_clips)
         audio_composite = CompositeAudioClip([audio_concat])
 
+
     # Gather all images
     image_clips = []
-    if os.path.exists("assets/mp3/posttext.mp3"):
-        image_clips.insert(
-            0,
-            ImageClip("assets/png/title.png")
-            .set_duration(audio_clips[0].duration + audio_clips[1].duration)
+    for i in range(0, number_of_clips):
+        image_clips.append(
+            ImageClip(f"assets/png/comment_{i}.png")
+            .set_duration(audio_clips[i + 1].duration)
             .set_position("center")
             .resize(width=1080 - 100)
             .set_opacity(float(opacity)),
         )
-    else:
-        image_clips.insert(
-            0,
-            ImageClip("assets/png/title.png")
-            .set_duration(audio_clips[0].duration)
-            .set_position("center")
-            .resize(width=1080 - 100)
-            .set_opacity(float(opacity)),
-        )
+
 
     image_concat = concatenate_videoclips(image_clips).set_position(("center", "center"))
     image_concat.audio = audio_composite
     final = CompositeVideoClip([background_clip, image_concat])
 
     if file_name is None:
-        filename = (
-            re.sub('[?\"%*:|<>]', '', ("assets/" + reddit.subreddit.submission.title + ".mp4"))
+        filename =  re.sub(
+            '[?\"%*:|<>]', '', (f"assets/{reddit.subreddit.submission.title}.mp4")
         )
 
     final.write_videofile(filename, fps=30, audio_codec="aac", audio_bitrate="192k")
-    for i in range(0, number_of_clips):
-        pass
