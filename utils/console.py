f6f45a6fd45d6b606ddbf3d819a3381b04e172b0
iaacornus <iaacornus.devel@gmail.com>
2022-06-07 19:49:16 +0800

    add backstyle, but preserved the dynamic styling via conditions

--- utils/console.py
+++ utils/console.py
@@ -22,7 +22,9 @@ def print_step(text):
     console.print(panel)
 
 
-def print_substep(text):
+def print_substep(text, style_=None):
     """Prints a rich info message without the panelling."""
 
+    if style_ is not None:
+        console.print(text, style=style_)
     console.print(text)
