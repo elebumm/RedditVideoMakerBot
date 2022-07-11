312d25c680576502cd47fe226138c994373e316a
anthony lloyd <contact@anthonylloyd.net>
2022-07-11 20:38:40 +1000

    minimum comments user defined variable

--- utils/subreddit.py
+++ utils/subreddit.py
@@ -34,8 +34,14 @@ def get_subreddit_undone(submissions: list, subreddit):
         if submission.stickied:
             print_substep("This post was pinned by moderators. Skipping...")
             continue
-        if submission.num_comments < 100:
-            print_substep("This post has less than 100 comments. Skipping...")
+        if submission.num_comments < 0:
+            print_substep("This post has 0 comments. Skipping...")
+            continue
+        try:
+            if submission.num_comments < settings.config["reddit"]["thread"]["min_comments"]:
+                print_substep("This post has less than user defined minimum comments, Skipping...")
+                continue
+        except AttributeError:
             continue
         return submission
     print("all submissions have been done going by top submission order")
