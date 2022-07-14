891c9945fd02b7003dd7cf9633b9516d0490fe81
Jason <thebosssg19@gmail.com>
2022-07-13 21:55:32 -0400

    refactor: moved backgrounds to CONSTANTS.py

--- video_creation/background.py
+++ video_creation/background.py
@@ -10,41 +10,8 @@ from pytube import YouTube
 from pytube.cli import on_progress
 
 from utils import settings
+from utils.CONSTANTS import background_options
 from utils.console import print_step, print_substep
-
-# Supported Background. Can add/remove background video here....
-# <key>-<value> : key -> used as keyword for TOML file. value -> background configuration
-# Format (value):
-# 1. Youtube URI
-# 2. filename
-# 3. Citation (owner of the video)
-# 4. Position of image clips in the background. See moviepy reference for more information. (https://zulko.github.io/moviepy/ref/VideoClip/VideoClip.html#moviepy.video.VideoClip.VideoClip.set_position)
-background_options = {
-    "motor-gta": (  # Motor-GTA Racing
-        "https://www.youtube.com/watch?v=vw5L4xCPy9Q",
-        "bike-parkour-gta.mp4",
-        "Achy Gaming",
-        lambda t: ("center", 480 + t),
-    ),
-    "rocket-league": (  # Rocket League
-        "https://www.youtube.com/watch?v=2X9QGY__0II",
-        "rocket_league.mp4",
-        "Orbital Gameplay",
-        lambda t: ("center", 200 + t),
-    ),
-    "minecraft": (  # Minecraft parkour
-        "https://www.youtube.com/watch?v=n_Dv4JMiwK8",
-        "parkour.mp4",
-        "bbswitzer",
-        "center",
-    ),
-    "gta": (  # GTA Stunt Race
-        "https://www.youtube.com/watch?v=qGa9kWREOnE",
-        "gta-stunt-race.mp4",
-        "Achy Gaming",
-        lambda t: ("center", 480 + t),
-    ),
-}
 
 
 def get_start_and_end_times(video_length: int, length_of_clip: int) -> Tuple[int, int]:
