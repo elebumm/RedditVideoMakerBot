e0a7d0f92f9ec547aec88c6af0d42328128c2c53
BlockArchitech <hello@blockarchitech.com>
2022-08-11 13:14:48 -0400

    fix: install.sh might break python environments
    I should probably also fix my git because I hate making commits on GitHub.
    
    Signed-off-by: BlockArchitech <hello@blockarchitech.com>

--- install.sh
+++ install.sh
@@ -62,6 +62,7 @@ function install_macos(){
     fi
     # Install the required packages
     echo "Installing required Packages" 
+
     if [! command --version python3 &> /dev/null ]; then
 	    echo "Installing python3"
 	    brew install python@3.10
