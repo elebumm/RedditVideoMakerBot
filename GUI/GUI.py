3ea46d4f7a154f0962e53cec399f6f9456fd11a0
RapidStoned <111416165+VirtualRabb@users.noreply.github.com>
2022-08-24 15:52:51 +0200

    To remove #test from gui.py

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -8,7 +8,6 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-#test
 class App(customtkinter.CTk):
 
     WIDTH = 1200
