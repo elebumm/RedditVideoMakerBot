83433cfaafd0ba29064bf8ec8d5bcd708197716b
João Victor SDV <trovaobrwot@gmail.com>
2022-10-12 13:34:25 -0300

    correcting filename length ffmpeg no such file#err

--- video_creation/final_video.py
+++ video_creation/final_video.py
@@ -161,7 +161,7 @@ def make_final_video(
     title = re.sub(r"[^\w\s-]", "", reddit_obj["thread_title"])
     idx = re.sub(r"[^\w\s-]", "", reddit_obj["thread_id"])
 
-    filename = f"{name_normalize(title)[:251]}"
+    filename = f"{name_normalize(title)[:150]}"
     subreddit = settings.config["reddit"]["thread"]["subreddit"]
 
     if not exists(f"./results/{subreddit}"):
