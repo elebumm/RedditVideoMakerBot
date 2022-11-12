67f42f7898c467b5dd510b7433e977d358ce3c9f
electro199 <amanraza626@gmail.com>
2022-11-12 11:12:48 -0800

    removed the beta test message

--- utils/version.py
+++ utils/version.py
@@ -9,11 +9,7 @@ def checkversion(__VERSION__:str):
     if __VERSION__ == latestversion:
         print_step(f"You are using the newest version ({__VERSION__}) of the bot")
         return True
-    #FOR ERROR HANDLING, SO VERSION CAN EASILY BE DETECTED
     else:
-         print_step(f"You are using the test version ({__VERSION__}) of the bot from AMAN RAZA")
-         #to be removed before released
-    # else:
-    #     print_step(
-    #         f"You are using an older version ({__VERSION__}) of the bot. Download the newest version ({latestversion}) from https://github.com/elebumm/RedditVideoMakerBot/releases/latest"
-    #     )
+        print_step(
+            f"You are using an older version ({__VERSION__}) of the bot. Download the newest version ({latestversion}) from https://github.com/elebumm/RedditVideoMakerBot/releases/latest"
+        )
