b137bb615579477219e59419e7d64a51abae91fc
Simon <65854503+OpenSourceSimon@users.noreply.github.com>
2023-02-13 17:00:17 +0100

    Update video_creation/screenshot_downloader.py

--- video_creation/screenshot_downloader.py
+++ video_creation/screenshot_downloader.py
@@ -38,7 +38,7 @@ def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
     with sync_playwright() as p:
         print_substep("Launching Headless Browser...")
 
-        browser = p.chromium.launch()  # headless=False for debugging
+        browser = p.chromium.launch(headless=True)  # headless=False will show the browser for debugging purposes
         context = browser.new_context()
         # Device scale factor (or dsf for short) allows us to increase the resolution of the screenshots
         # When the dsf is 1, the width of the screenshot is 600 pixels
