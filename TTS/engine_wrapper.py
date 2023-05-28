bb65d2f6cdda888ba69f3ad4a60bbd9b1eac9eb4
Simon <65854503+OpenSourceSimon@users.noreply.github.com>
2023-05-28 15:09:04 +0200

    Update TTS/engine_wrapper.py

--- TTS/engine_wrapper.py
+++ TTS/engine_wrapper.py
@@ -17,7 +17,7 @@ from utils import settings
 from utils.console import print_step, print_substep
 from utils.voice import sanitize_text
 
-DEFAULT_MAX_LENGTH: int = 29  # video length variable
+DEFAULT_MAX_LENGTH: int = 50  # Video length variable, edit this on your own risk. It should work, but it's not supported
 
 
 class TTSEngine:
