e45d298e809603e525a88ee7513793aa9a92863b
sajawalhassan <sajawalhassan.1feb@gmail.com>
2023-06-12 11:45:39 +0500

    fix console output #2

--- generate_long_video.py
+++ generate_long_video.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from utils import settings
+from video_generator import VideoGenerator
 import sys
+
+abs_directory = Path().absolute()
+config = settings.check_toml(f"{abs_directory}/utils/.config.template.toml", f"{abs_directory}/config.toml")
 
 num_of_vids_to_be_conc = input("How many videos would you like to be concatenated into a single video? ")
 try:
@@ -16,13 +20,8 @@ else:
     if remove_past_results == "y": remove_past_results = True
     if remove_past_results == "n": remove_past_results = False
 
-abs_directory = Path().absolute()
-config = settings.check_toml(f"{abs_directory}/utils/.config.template.toml", f"{abs_directory}/config.toml")
-
 config["settings"]["resolution_w"] = 1920
 config["settings"]["resolution_h"] = 1080
-
-from video_generator import VideoGenerator
 
 video_gen = VideoGenerator(config)
 video_gen.generate_long_video(num_of_vids_to_be_conc, remove_past_results)
