97286d94f42818f9d144615f5ac6ebd433759bec
Jayden  Elishaw <80910384+BluJay131@users.noreply.github.com>
2023-10-12 14:15:14 -0700

    Update StartAll.bat

--- StartAll.bat
+++ StartAll.bat
@@ -1,3 +1,18 @@
 cd C:\Users\JAdmin\Desktop\RYBot
+
+@echo off
+set VENV_DIR=.venv
+
+if exist "%VENV_DIR%" (
+    echo Activating virtual environment...
+    call "%VENV_DIR%\Scripts\activate.bat"
+)
+
+echo Running Python script...
 python main.py
 python upload.py
+
+if errorlevel 1 (
+    echo An error occurred. Press any key to exit.
+    pause >nul
+)
