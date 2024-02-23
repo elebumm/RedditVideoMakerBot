8166668989fa6c8c823deed8585fc382662f1612
Basti <75910510+RqZen@users.noreply.github.com>
2024-02-23 21:23:34 +0100

    streamlabspolly_fix.py
    fixes the streamlabspolly error:
    
    Error occurred calling Streamlabs Polly

--- TTS/streamlabs_polly.py
+++ TTS/streamlabs_polly.py
@@ -44,7 +44,8 @@ class StreamlabsPolly:
                 )
             voice = str(settings.config["settings"]["tts"]["streamlabs_polly_voice"]).capitalize()
         body = {"voice": voice, "text": text, "service": "polly"}
-        response = requests.post(self.url, data=body)
+        headers = {"Referer" : "https://streamlabs.com/" }
+        response = requests.post(self.url,  headers=headers,data=body)
         if not check_ratelimit(response):
             self.run(text, filepath, random_voice)
 
