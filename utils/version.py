32ea6560c446c3132d11567210db65031f675482
Rafael de Bem <debemrafael@gmail.com>
2024-03-01 22:50:45 -0300

    bug(#1974): verify GitHub API response before trying to access the JSON dictionary

--- utils/version.py
+++ utils/version.py
@@ -7,6 +7,11 @@ def checkversion(__VERSION__: str):
     response = requests.get(
         "https://api.github.com/repos/elebumm/RedditVideoMakerBot/releases/latest"
     )
+    if response.status_code == 403:  # Rate limit
+        print(
+            f"Skipping script version check because we exceeded GitHub's rate limit. Using version ({__VERSION__})."
+        )
+        return
     latestversion = response.json()["tag_name"]
     if __VERSION__ == latestversion:
         print_step(f"You are using the newest version ({__VERSION__}) of the bot")
