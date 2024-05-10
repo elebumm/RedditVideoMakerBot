077db3a57a53d95daf1ff670290b4896b6bba82b
cyteon <cyteon>
2024-05-10 20:00:25 +0200

    Can now change the name that appears in the fancy start title

--- video_creation/final_video.py
+++ video_creation/final_video.py
@@ -121,7 +121,7 @@ def create_fancy_thumbnail(image, text, text_color, padding, wrap=35):
     draw = ImageDraw.Draw(image)
 
     username_font = ImageFont.truetype(os.path.join("fonts", "Roboto-Bold.ttf"), 30)
-    draw.text((205, 825), f"Reddit Tales", font=username_font, fill=text_color, align="left")
+    draw.text((205, 825), settings.config["settings"]["channel_name"], font=username_font, fill=text_color, align="left")
 
     if len(lines) == 3:
         lines = textwrap.wrap(text, width=wrap+10)
