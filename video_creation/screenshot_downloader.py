901ad75e0b8e4daf7299551c39eb68e5c665e052
Jason <git@jasoncameron.dev>
2024-06-16 16:44:47 -0400

    Fixed installation error by specifying non 2.0 numpy + incorrect dunder all

--- video_creation/screenshot_downloader.py
+++ video_creation/screenshot_downloader.py
@@ -13,7 +13,7 @@ from utils.imagenarator import imagemaker
 from utils.playwright import clear_cookie_by_name
 from utils.videos import save_data
 
-__all__ = ["download_screenshots_of_reddit_posts"]
+__all__ = ["get_screenshots_of_reddit_posts"]
 
 
 def get_screenshots_of_reddit_posts(reddit_object: dict, screenshot_num: int):
